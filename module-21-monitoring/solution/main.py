e7aaa9041fb49b5fd98ba1d7bf49f5ed1ba9f382
Amal Feriani <amal.feriani@gmail.com>
2026-05-12 20:25:16 -0400

    update module 21 solution

--- module-21-monitoring/solution/main.py
+++ module-21-monitoring/solution/main.py
@@ -8,7 +8,7 @@ from prometheus_client import Counter, Histogram
 from prometheus_fastapi_instrumentator import Instrumentator
 from transformers import pipeline
 
-#TODO Add histogram that tracks the distribution of model prediction confidence scores per request
+# TODO: Add histogram that tracks the distribution of model prediction confidence scores per request
 
 prediction_confidence = Histogram(
     "model_prediction_confidence",
@@ -16,7 +16,7 @@ prediction_confidence = Histogram(
     buckets=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
 )
 
-#TODO Add a Prometheus counter that tracks the count of predictions per output class
+# TODO: Add a Prometheus counter that tracks the count of predictions per output class
 prediction_class_counter = Counter(
     "model_prediction_class_total",
     "Number of predictions per output class",
@@ -40,7 +40,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Beans Disease Classifier API", lifespan=lifespan)
 
-#TODO Add Prometheus instrumentation to the FastAPI app via instrumentator
+# TODO: Add Prometheus instrumentation to the FastAPI app via instrumentator
+# https://pypi.org/project/prometheus-fastapi-instrumentator/
+# No need for implementing /metrics endpoint, it will be automatically added by the instrumentator
 Instrumentator().instrument(app).expose(app)
 
 
@@ -60,13 +62,14 @@ async def predict(file: UploadFile = File(...)):
     image = Image.open(io.BytesIO(contents)).convert("RGB")
     predictions = classifier(image)
 
+    #TODO extract the top prediction's confidence score and label
     top = predictions[0]
     label = top["label"]
     score = float(top["score"])
 
-    #TODO Observe the prediction confidence score in the histogram
+    # TODO: Observe the prediction confidence score in the histogram
     prediction_confidence.observe(score)
-    #TODO Increment the counter for the predicted class label
+    # TODO: Increment the counter for the predicted class label
     prediction_class_counter.labels(class_name=label).inc()
 
     return {"label": label, "score": score}
